9fec46fad817ce5ecfc6316f20b1a0c27b013260
PoojaVaswani1 <141347236+PoojaVaswani1@users.noreply.github.com>
2023-11-28 21:59:48 +0530

    Add files via upload

---
+++ Java/28-11/ReverseTriangleNo.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class ReverseTriangleNo {
+	public static void main(String[] args) {
+        nForest(4);
+    }
+   public static void nForest(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++){
+	    for(int j=0;j<=n-i;j++){
+	         System.out.print(j);
+             
+
+	}
+	      System.out.println();
+
+	}
+    }
+
+}
+
