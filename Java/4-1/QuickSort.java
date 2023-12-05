0f454d457e507d09db8a06a3eff55c06bcdc9712
PoojaVaswani1 <141347236+PoojaVaswani1@users.noreply.github.com>
2023-12-05 13:21:07 +0530

    Add files via upload

--- Java/4-1/QuickSort.java
+++ Java/4-1/QuickSort.java
@@ -2,10 +2,7 @@ package Sorting;
 
 public class QuickSort  
 {  
-    /* function that consider last element as pivot,  
-place the pivot at its exact position, and place  
-smaller elements to left of pivot and greater  
-elements to right of pivot.  */  
+    
 int partition (int a[], int start, int end)  
 {  
     int pivot = a[end]; // pivot element  
@@ -13,10 +10,10 @@ int partition (int a[], int start, int end)
   
     for (int j = start; j <= end - 1; j++)  
     {  
-        // If current element is smaller than the pivot  
+         
         if (a[j] < pivot)  
         {  
-            i++; // increment index of smaller element  
+            i++;  
             int t = a[i];  
             a[i] = a[j];  
             a[j] = t;  
@@ -28,18 +25,18 @@ int partition (int a[], int start, int end)
     return (i + 1);  
 }  
   
-/* function to implement quick sort */  
-void quick(int a[], int start, int end) /* a[] = array to be sorted, start = Starting index, end = Ending index */  
+
+void quick(int a[], int start, int end) 
 {  
     if (start < end)  
     {  
-        int p = partition(a, start, end);  //p is partitioning index  
+        int p = partition(a, start, end);   
         quick(a, start, p - 1);  
         quick(a, p + 1, end);  
     }  
 }  
   
-/* function to print an array */  
+ 
 void printArr(int a[], int n)  
 {  
     int i;  
@@ -50,7 +47,7 @@ void printArr(int a[], int n)
     int a[] = { 13, 18, 27, 2, 19, 25 };  
     int n = a.length;  
     System.out.println("\nBefore sorting array elements are - ");  
-    Quick q1 = new Quick();  
+    QuickSort q1 = new QuickSort();  
     q1.printArr(a, n);  
     q1.quick(a, 0, n - 1);  
     System.out.println("\nAfter sorting array elements are - ");  
